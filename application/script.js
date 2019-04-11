fb73543e38b40f293f187be1faebf8340debd30d
FlaviusFlavian <48382423+FlaviusFlavian@users.noreply.github.com>
2019-04-11 18:34:45 +0300

    Add files via upload
    Ultimele modificari facute chiar inainte de prezentare.

--- application/script.js
+++ application/script.js
@@ -1,33 +1,17 @@
 var slideNo = 0;
-var slideTimeout = 4100;
-var slideshowImages = ["assets/images/slide_1.jpg",
-	"assets/images/slide_2.jpg",
-	"assets/images/slide_3.jpg"];
-var slideshowHeaders = ["Protect what is yours.",
-			"Know the latest security threats.",
-			"Learn how to engineer \"hackproof\" systems."];
-/* Image source (slide_1.jpg): https://www.telindus.nl/mircosegmentatie-software-defined-datacenter/ 
-	Image source (slide_2.jpg): https://www.compli.com/blog/the-best-defense-against-cyber-attacks/
-	Image source (slide_3.jpg): https://wallpaperplay.com/board/java-programming-wallpapers */
-
-	slideshow();
+slideshow();
 
 function slideshow() {
-	var slide = document.getElementById("slide");
-	slide.classList.add("fadeOut");
-	setTimeout(function() {
-		document.getElementById("slide").querySelector("img").src = 
-			slideshowImages[slideNo];
-		document.getElementById("slide").querySelector("h2").innerHTML = 
-			slideshowHeaders[slideNo];
-		slide.classList.remove("fadeOut");
-	}, 1600);
+	var slides = document.getElementsByClassName("slide");
 
-	slideNo++;
+	for (var i = 0; i < slides.length; i++)
+		slides[i].style.display = "none";
 
-	if (slideNo == slideshowImages.length)
+	if (slideNo >= slides.length)
 		slideNo = 0;
 
-	setTimeout(slideshow, slideTimeout);
+	slides[slideNo].style.display = "flex";
+	slideNo++;
+	setTimeout(slideshow, 3500);
 }
 
