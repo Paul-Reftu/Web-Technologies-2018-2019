119a625b219e597822308e93944cce574c4c5ed8
RuseDanielStefan <48377557+RuseDanielStefan@users.noreply.github.com>
2019-04-17 20:54:25 +0300

    Update Header.php

--- application/Header.php
+++ application/Header.php
@@ -4,7 +4,7 @@
 
 		public function __construct() {
 			echo "<header id=\"main-header\">";
-			echo "<a href=\"index.php\"><img src=\"assets/images/logo.png\" width=90% height=60% /></a>";
+			echo "<a href=\"index.php\"><img src=\"assets/images/logo.png\" style ="width:90%; height:60%;" /></a>";
 			echo "<h1>Security Alerter</h1>";
 			echo "</header>";
 		}
